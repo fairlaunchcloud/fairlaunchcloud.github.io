1ac072b3a6aed93e1cd15b771540e02a4d146888
fairlaunchcloud <fairlaunchcloud@gmail.com>
2025-02-18 17:33:16 +0800

    update

--- 0xD8C7f796025649e7C56e6f3d2E2841354AAcdBf0/app.js
+++ 0xD8C7f796025649e7C56e6f3d2E2841354AAcdBf0/app.js
@@ -1617,6 +1617,188 @@ const contractABI =
 		"inputs": [
 			{
 				"internalType": "address",
+				"name": "_extraAddress",
+				"type": "address"
+			}
+		],
+		"name": "setExtraAddress",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_extraSharePercentage",
+				"type": "uint256"
+			}
+		],
+		"name": "setExtraSharePercentage",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_LockToken",
+				"type": "address"
+			}
+		],
+		"name": "setLockToken",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_lpAddress",
+				"type": "address"
+			}
+		],
+		"name": "setLpAddress",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_lpSharePercentage",
+				"type": "uint256"
+			}
+		],
+		"name": "setLpSharePercentage",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_ownerSharePercentage",
+				"type": "uint256"
+			}
+		],
+		"name": "setOwnerSharePercentage",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_partnerAddress",
+				"type": "address"
+			}
+		],
+		"name": "setPartnerAddress",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_PayTokenA",
+				"type": "address"
+			}
+		],
+		"name": "setPayTokenA",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_PayTokenB",
+				"type": "address"
+			}
+		],
+		"name": "setPayTokenB",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_PayTokenC",
+				"type": "address"
+			}
+		],
+		"name": "setPayTokenC",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_PayTokenD",
+				"type": "address"
+			}
+		],
+		"name": "setPayTokenD",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_PayTokenE",
+				"type": "address"
+			}
+		],
+		"name": "setPayTokenE",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_projectAddress",
+				"type": "address"
+			}
+		],
+		"name": "setProjectAddress",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_projectSharePercentage",
+				"type": "uint256"
+			}
+		],
+		"name": "setProjectSharePercentage",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
 				"name": "contributor",
 				"type": "address"
 			},
@@ -1627,6 +1809,695 @@ const contractABI =
 			}
 		],
 		"name": "setReferral",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralSharePercentage",
+				"type": "uint256"
+			}
+		],
+		"name": "setReferralSharePercentage",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rewardAmountTokenA",
+				"type": "uint256"
+			}
+		],
+		"name": "setRewardAmountTokenA",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rewardAmountTokenAforLp",
+				"type": "uint256"
+			}
+		],
+		"name": "setRewardAmountTokenAforLp",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rewardAmountTokenAforPartner",
+				"type": "uint256"
+			}
+		],
+		"name": "setRewardAmountTokenAforPartner",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rewardAmountTokenB",
+				"type": "uint256"
+			}
+		],
+		"name": "setRewardAmountTokenB",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rewardAmountTokenBforLp",
+				"type": "uint256"
+			}
+		],
+		"name": "setRewardAmountTokenBforLp",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rewardAmountTokenBforPartner",
+				"type": "uint256"
+			}
+		],
+		"name": "setRewardAmountTokenBforPartner",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rewardAmountTokenC",
+				"type": "uint256"
+			}
+		],
+		"name": "setRewardAmountTokenC",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rewardAmountTokenCforLp",
+				"type": "uint256"
+			}
+		],
+		"name": "setRewardAmountTokenCforLp",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rewardAmountTokenCforPartner",
+				"type": "uint256"
+			}
+		],
+		"name": "setRewardAmountTokenCforPartner",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rewardAmountTokenD",
+				"type": "uint256"
+			}
+		],
+		"name": "setRewardAmountTokenD",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rewardAmountTokenDforLp",
+				"type": "uint256"
+			}
+		],
+		"name": "setRewardAmountTokenDforLp",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rewardAmountTokenDforPartner",
+				"type": "uint256"
+			}
+		],
+		"name": "setRewardAmountTokenDforPartner",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rewardAmountTokenE",
+				"type": "uint256"
+			}
+		],
+		"name": "setRewardAmountTokenE",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rewardAmountTokenEforLp",
+				"type": "uint256"
+			}
+		],
+		"name": "setRewardAmountTokenEforLp",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rewardAmountTokenEforPartner",
+				"type": "uint256"
+			}
+		],
+		"name": "setRewardAmountTokenEforPartner",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_RewardTokenA",
+				"type": "address"
+			}
+		],
+		"name": "setRewardTokenA",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_RewardTokenB",
+				"type": "address"
+			}
+		],
+		"name": "setRewardTokenB",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_RewardTokenC",
+				"type": "address"
+			}
+		],
+		"name": "setRewardTokenC",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_RewardTokenD",
+				"type": "address"
+			}
+		],
+		"name": "setRewardTokenD",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_RewardTokenE",
+				"type": "address"
+			}
+		],
+		"name": "setRewardTokenE",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_teamDevAddress",
+				"type": "address"
+			}
+		],
+		"name": "setTeamDevAddress",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_teamDevSharePercentage",
+				"type": "uint256"
+			}
+		],
+		"name": "setTeamDevSharePercentage",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralPercentagesGen1",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralPercentagesGen1",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralPercentagesGen2",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralPercentagesGen2",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralPercentagesGen3",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralPercentagesGen3",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralPercentagesGen4",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralPercentagesGen4",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralPercentagesGen5",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralPercentagesGen5",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenAGen1",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenAGen1",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenAGen2",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenAGen2",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenAGen3",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenAGen3",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenAGen4",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenAGen4",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenAGen5",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenAGen5",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenBGen1",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenBGen1",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenBGen2",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenBGen2",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenBGen3",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenBGen3",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenBGen4",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenBGen4",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenBGen5",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenBGen5",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenCGen1",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenCGen1",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenCGen2",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenCGen2",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenCGen3",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenCGen3",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenCGen4",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenCGen4",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenCGen5",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenCGen5",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenDGen1",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenDGen1",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenDGen2",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenDGen2",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenDGen3",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenDGen3",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenDGen4",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenDGen4",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenDGen5",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenDGen5",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenEGen1",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenEGen1",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenEGen2",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenEGen2",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenEGen3",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenEGen3",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenEGen4",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenEGen4",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_referralRewardTokenEGen5",
+				"type": "uint256"
+			}
+		],
+		"name": "setreferralRewardTokenEGen5",
 		"outputs": [],
 		"stateMutability": "nonpayable",
 		"type": "function"
