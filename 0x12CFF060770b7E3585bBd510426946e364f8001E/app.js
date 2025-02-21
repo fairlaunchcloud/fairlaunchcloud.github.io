8c510b9ff8e8124a55d70e8ac44dba06a752c9b9
fairlaunchcloud <fairlaunchcloud@gmail.com>
2025-02-21 14:54:10 +0800

    update

--- 0x12CFF060770b7E3585bBd510426946e364f8001E/app.js
+++ 0x12CFF060770b7E3585bBd510426946e364f8001E/app.js
@@ -1,7 +1,7 @@
 const CHAIN_ID = "0x89"; 
 
 // Ganti dengan alamat smart contract
-const contractAddress ="0x12CFF06070b7E3585bBd510426946e364f8001E";
+const contractAddress ="0x12CFF060770b7E3585bBd510426946e364f8001E";
 const contractABI = [
 	{
 		"inputs": [
@@ -2500,7 +2500,7 @@ const TOKEN_ADDRESSES = {
   };
 
 // Alamat referral default
-const DEFAULT_REFERRAL_ADDRESS = "0x12CFF06070b7E3585bBd510426946e364f8001E";
+const DEFAULT_REFERRAL_ADDRESS = "0x12CFF060770b7E3585bBd510426946e364f8001E";
 document.getElementById("contractFairlaunch").textContent = contractAddress;
 // Inisialisasi provider dan signer
 let provider, signer, contract;
