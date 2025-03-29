550301881c54b35f150f012e8bb345eaf48544d1
fairlaunchcloud <fairlaunchcloud@gmail.com>
2025-03-29 13:00:18 +0800

    update

--- bullclub/app.js
+++ bullclub/app.js
@@ -31,6 +31,13 @@ const modal = createAppKit({
         onramp: false,
         swaps: false,
     },
+    featuredWalletIds: [
+        'bb71b54ced62aa11f76e4f3edacb37a41300807506db840b98b740379f99cc71',
+        '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
+        'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
+        '38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662',
+        '20459438007b75f4f4acb98bf29aa3b800550309646d375da5fd4aac6c2a2c66'
+      ],
     metadata: {
         name: 'Fairlaunch',
         description: 'Fairlaunch Community',
