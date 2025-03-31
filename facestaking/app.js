b8ff9f06d61497b7e9e095369ebb10611b24379f
fairlaunchcloud <fairlaunchcloud@gmail.com>
2025-03-31 11:01:04 +0800

    update

--- facestaking/app.js
+++ facestaking/app.js
@@ -24,6 +24,9 @@ const modal = createAppKit({
     enableWalletConnect: true,
     projectId,
     themeMode: 'dark',
+    themeVariables: {
+        '--w3m-accent': '#91d3ff',
+    },
     features: {
         analytics: true,
         email: false,
