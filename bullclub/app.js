a418b21f08c5e11eb787fb1c926c6dbf534ee71d
fairlaunchcloud <fairlaunchcloud@gmail.com>
2025-03-31 10:44:27 +0800

    update

--- bullclub/app.js
+++ bullclub/app.js
@@ -24,6 +24,9 @@ const modal = createAppKit({
     enableWalletConnect: true,
     projectId,
     themeMode: 'dark',
+    themeVariables: {
+        '--w3m-accent': '#bea099',
+    },
     features: {
         analytics: true,
         email: false,
