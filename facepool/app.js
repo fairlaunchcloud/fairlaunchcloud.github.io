bf4aef64c5c8edb2fc1b6aa4389da2d12c1a33e7
fairlaunchcloud <fairlaunchcloud@gmail.com>
2025-03-31 11:06:51 +0800

    update

--- facepool/app.js
+++ facepool/app.js
@@ -24,6 +24,9 @@ const modal = createAppKit({
     enableWalletConnect: true,
     projectId,
     themeMode: 'dark',
+    themeVariables: {
+        '--w3m-accent': '#d290f5',
+    },
     features: {
         analytics: true,
         email: false,
