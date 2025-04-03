ea4d4850add78e2049de7dbfbbbf80a25d1b2a01
fairlaunchcloud <fairlaunchcloud@gmail.com>
2025-04-04 04:38:43 +0800

    update

--- ENDY/app.js
+++ ENDY/app.js
@@ -25,7 +25,7 @@ const modal = createAppKit({
     projectId,
     themeMode: 'dark',
     themeVariables: {
-        '--w3m-accent': '#3fc3ee',
+        '--w3m-accent': '#ffd700',
     },
     features: {
         analytics: true,
