cd8426f64593df38797c7906158116951d8aae09
fairlaunchcloud <fairlaunchcloud@gmail.com>
2026-01-31 00:23:50 +0800

    update

--- CMiiner/main.js
+++ CMiiner/main.js
@@ -1,4 +1,4 @@
-let countDownDate = new Date('Nov 03, 2025 00:00:00').getTime();
+let countDownDate = new Date('Jan 31, 2026 00:00:00').getTime();
 
 let x = setInterval(function () {
   let now = new Date().getTime();
