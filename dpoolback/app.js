2ede8ceac3994d91f5c1ecc1ab06c83fae102a88
fairlaunchcloud <fairlaunchcloud@gmail.com>
2026-05-26 10:55:58 +0800

    update

--- dpoolback/app.js
+++ dpoolback/app.js
@@ -25,7 +25,7 @@ const modal = createAppKit({
     projectId,
     themeMode: 'dark',
     themeVariables: {
-        '--w3m-accent': 'aqua',
+        '--w3m-accent': 'royalblue',
     },
     features: {
         analytics: true,
