aeeddbf499cd450656d31f5c6991489dc6232dbf
fairlaunchcloud <fairlaunchcloud@gmail.com>
2026-06-01 19:29:31 +0800

    update

--- xpoolsociety/app.js
+++ xpoolsociety/app.js
@@ -25,7 +25,7 @@ const modal = createAppKit({
     projectId,
     themeMode: 'dark',
     themeVariables: {
-        '--w3m-accent': 'aqua',
+        '--w3m-accent': 'lightcoral',
     },
     features: {
         analytics: true,
