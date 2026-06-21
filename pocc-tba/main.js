d161f9392b186c6e01bcb90e3c3ce33db9c01c43
fairlaunchcloud <fairlaunchcloud@gmail.com>
2026-06-21 12:10:33 +0800

    update

--- pocc-tba/main.js
+++ pocc-tba/main.js
@@ -1,4 +1,4 @@
-let countDownDate = new Date('JUNE 16, 2026 00:17:00').getTime();
+let countDownDate = new Date('JUNE 23, 2026 02:10:00').getTime();
 
 let x = setInterval(function () {
   let now = new Date().getTime();
